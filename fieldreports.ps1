4b441e8718d8a75d03d0c80874515c7e0c090381
Martin Cevallos <mcevallos@sdl.com>
2019-06-10 16:49:10 -0400

    Update fieldreports.ps1

--- fieldreports.ps1
+++ fieldreports.ps1
@@ -1,18 +1,18 @@
-Enable-PSRemoting
+    Enable-PSRemoting
 
-$Credential = $host.ui.PromptForCredential("Oradev login", "Please enter your password.", "oradev\mcevallo", "NetBiosUserName")
+$Credential = $host.ui.PromptForCredential("domIN login", "Please enter your password.", "DOMAIN/USR", "NetBiosUserName")
 
 
 #Prod
 Invoke-Command -ComputerName adc01lri -Credential $Credential {
-    Get-IshTypeFieldDefinition -IshSession (New-IshSession -WsBaseUrl "https://ccms.us.oracle.com/infosharews" -PSCredential "Martin Cevallos") -TriDKXmlSetupFilePath "C:\InfoShare\App\Database\Common\DatabaseIndependent\Examples\Full-Export\exportInfoshare_result.xml"
+    Get-IshTypeFieldDefinition -IshSession (New-IshSession -WsBaseUrl "https:///infosharews" -PSCredential "") -TriDKXmlSetupFilePath "C:\InfoShare\App\Database\Common\DatabaseIndependent\Examples\Full-Export\exportInfoshare_result.xml"
 } | Out-GridView
 #Int
 Invoke-Command -ComputerName adc01eam -Credential $Credential {
-    Get-IshTypeFieldDefinition -IshSession (New-IshSession -WsBaseUrl "https://ccms-int.us.oracle.com/infosharews" -PSCredential "Martin Cevallos") -TriDKXmlSetupFilePath "C:\InfoShare\App\Database\Common\DatabaseIndependent\Examples\Full-Export\exportInfoshare_result.xml"
+    Get-IshTypeFieldDefinition -IshSession (New-IshSession -WsBaseUrl "https:///infosharews" -PSCredential "") -TriDKXmlSetupFilePath "C:\InfoShare\App\Database\Common\DatabaseIndependent\Examples\Full-Export\exportInfoshare_result.xml"
 } | Out-GridView
 #Dev
 Invoke-Command -ComputerName adc01lrr -Credential $Credential {
-    Get-IshTypeFieldDefinition -IshSession (New-IshSession -WsBaseUrl "https://ccms-dev.us.oracle.com/infosharews" -PSCredential "Martin Cevallos") -TriDKXmlSetupFilePath "C:\InfoShare\App\Database\Common\DatabaseIndependent\Examples\Full-Export\exportInfoshare_result.xml"
+    Get-IshTypeFieldDefinition -IshSession (New-IshSession -WsBaseUrl "https:///infosharews" -PSCredential "") -TriDKXmlSetupFilePath "C:\InfoShare\App\Database\Common\DatabaseIndependent\Examples\Full-Export\exportInfoshare_result.xml"
 } | Out-GridView
 
